d0dfaee59dcfc08d1a47da8900784344fcd3d0fb
tpt2213 <tpt2213@gmail.com>
2018-06-06 12:13:30 +0700

    re init

--- api/db/migrations/05312018_status.js
+++ api/db/migrations/05312018_status.js
@@ -4,13 +4,9 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTableIfNotExists('status', table => {
     table.increments();
     table.string('name').notNullable();
-  }).insert([
-    {id: 1, name: 'approve'},
-    {id: 2, name: 'pending'},
-    {id: 3, name: 'deny'}
-  ]);
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('status');
+  return knex.schema.dropTableIfExists('status');
 };
--- api/db/migrations/05312018_type_of_place.js
+++ api/db/migrations/05312018_type_of_place.js
@@ -4,14 +4,9 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTableIfNotExists('typeOfPlace', table => {
     table.increments();
     table.string('name').notNullable();
-  }).insert([
-    {id: 1, name: 'hospital'},
-    {id: 2, name: 'school'},
-    {id: 3, name: 'park'},
-    {id: 4, name: 'supermarket'},
-  ]);
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('typeOfPlace');
+  return knex.schema.dropTableIfExists('typeOfPlace');
 };
