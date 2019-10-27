ae23b8005d4d628c7dba6087817f35c0c9bd2093
Andrew <thanhtp3@fsoft.com.vn>
2019-10-27 08:05:00 +0400

    hide k

--- new-web/src/globals.ts
+++ new-web/src/globals.ts
@@ -3,5 +3,5 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Globals {
   API_URL: string = 'http://localhost:5577/';
-  GOOGLE_API_API_KEY: string = 'AIzaSyBjTgdelut1BnmVozi717aF0kW0g5UmaaM';
+  GOOGLE_API_API_KEY: string = '';
 }
